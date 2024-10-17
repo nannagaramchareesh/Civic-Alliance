66960d888e4c58858b185314c5de6ebf40690084
NANNAGARAM CHAREESH <22h51a05b1@cmrcet.ac.in>
2024-10-17 16:34:10 +0530

    commited

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Collab from './components/Collab'
+import Infopage from './components/Infopage'
+import Footer from './components/Footer'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar/>
+      <Hero/>
+      <Infopage/>
+      <Footer/>
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/components/Collab.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Marquee from "react-fast-marquee";
+
+export default function Collab() {
+    return (
+        <div>
+            <section className="bg-white dark:bg-gray-900">
+                <div className="max-w-screen-xl px-4 pb-8 mx-auto lg:pb-16">
+                    <div className="text-gray-500 dark:text-gray-400 overflow-hidden"> {/* Add overflow-hidden here */}
+                        <Marquee speed={100} style={{ overflow: 'hidden' }}> {/* You can also set overflow hidden here */}
+                            <div className='mr-32 group cursor-pointer'>
+                                <h1 className='text-gray-400 text-3xl font-bold text-center group-hover:text-white'>Ministry</h1>
+                                <h1 className='text-gray-400 text-3xl font-bold text-center group-hover:text-white'>Of Gas</h1>
+                            </div>
+
+                            <div className='mr-32 group cursor-pointer'>
+                                <h1 className='text-gray-400 text-3xl text-center font-bold group-hover:text-white'>Ministry</h1>
+                                <h1 className='text-gray-400 text-3xl font-bold text-center group-hover:text-white'>Of Roads</h1>
+                            </div>
+                            <div className='mr-32 group cursor-pointer'>
+                                <h1 className='text-gray-400 text-3xl text-center font-bold group-hover:text-white'>Ministry</h1>
+                                <h1 className='text-gray-400 text-3xl font-bold text-center group-hover:text-white'>Of Health</h1>
+                            </div>
+                            <div className='mr-32 group cursor-pointer'>
+                                <h1 className='text-gray-400 text-3xl text-center font-bold group-hover:text-white'>Ministry</h1>
+                                <h1 className='text-gray-400 text-3xl font-bold text-center group-hover:text-white'>Of Defence</h1>
+                            </div>
+                            <div className='mr-32 group cursor-pointer'>
+                                <h1 className='text-gray-400 text-3xl text-center font-bold group-hover:text-white'>Ministry</h1>
+                                <h1 className='text-gray-400 text-3xl font-bold text-center group-hover:text-white'>Of Finance</h1>
+                            </div>
+                        </Marquee>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
