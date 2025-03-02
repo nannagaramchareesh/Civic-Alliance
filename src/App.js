b20258b5888c10d36b84d369a85ba5d65cacb505
NANNAGARAM CHAREESH <22h51a05b1@cmrcet.ac.in>
2025-03-02 16:08:19 +0530

    sent signup request for department head

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Footer from './components/Footer'
 import Signup from './components/Signup'
+import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import Login from './components/Login'
 import AddProject from './components/AddProject'
 import Contact from './components/Contact'
 import Aboutus from './components/Aboutus'
 import ViewProjects from './components/ViewProjects'
+import { ToastContainer } from 'react-toastify'
 export default function App() {
   return (
     <div className='dark:bg-gray-900 bg-gradient-to-b from-blue-950/[.19] via-transparent'>
       <Router>
         <Navbar/>
+      <ToastContainer/>
         <Routes>
           <Route path='/' element={<Hero/>}/>
           <Route path='/signup' element={<Signup/>}/>
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useState } from "react";
 import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Signup() {
   const [formData, setFormData] = useState({
@@ -20,13 +21,15 @@ export default function Signup() {
   // Handle form submission
   const handleSubmit = async (e) => {
     e.preventDefault();
-
+    console.log(formData.firstName)
     try {
       const response = await axios.post("http://localhost:5000/api/departmentHead/signup", {
+        name:formData.firstName,
         email: formData.email,
         password: formData.password,
         department: formData.department,
       });
+      console.log(response.data)
       if(response.data.success){
         toast.success(response.data.message)
       }
@@ -47,10 +50,10 @@ export default function Signup() {
           className="max-w-4xl mx-auto bg-white shadow-[0_2px_13px_-6px_rgba(0,0,0,0.4)] sm:p-8 p-4 rounded-md"
           onSubmit={handleSubmit}
         >
-          <div class="grid md:grid-cols-2 gap-8">
+          <div className="grid md:grid-cols-2 gap-8">
             <button type="button"
-              class="w-full px-6 py-3 flex items-center justify-center rounded-md text-gray-800 text-sm tracking-wider font-semibold border-none outline-none bg-gray-100 hover:bg-gray-200">
-              <svg xmlns="http://www.w3.org/2000/svg" width="22px" fill="#fff" class="inline shrink-0 mr-4" viewBox="0 0 512 512">
+              className="w-full px-6 py-3 flex items-center justify-center rounded-md text-gray-800 text-sm tracking-wider font-semibold border-none outline-none bg-gray-100 hover:bg-gray-200">
+              <svg xmlns="http://www.w3.org/2000/svg" width="22px" fill="#fff" className="inline shrink-0 mr-4" viewBox="0 0 512 512">
                 <path fill="#fbbd00"
                   d="M120 256c0-25.367 6.989-49.13 19.131-69.477v-86.308H52.823C18.568 144.703 0 198.922 0 256s18.568 111.297 52.823 155.785h86.308v-86.308C126.989 305.13 120 281.367 120 256z"
                   data-original="#fbbd00" />
@@ -73,8 +76,8 @@ export default function Signup() {
               Continue with Google
             </button>
             <button type="button"
-              class="w-full px-6 py-3 flex items-center justify-center rounded-md text-white text-sm tracking-wider font-semibold border-none outline-none bg-black hover:bg-[#333]">
-              <svg xmlns="http://www.w3.org/2000/svg" width="22px" fill="#fff" class="inline shrink-0 mr-4" viewBox="0 0 22.773 22.773">
+              className="w-full px-6 py-3 flex items-center justify-center rounded-md text-white text-sm tracking-wider font-semibold border-none outline-none bg-black hover:bg-[#333]">
+              <svg xmlns="http://www.w3.org/2000/svg" width="22px" fill="#fff" className="inline shrink-0 mr-4" viewBox="0 0 22.773 22.773">
                 <path
                   d="M15.769 0h.162c.13 1.606-.483 2.806-1.228 3.675-.731.863-1.732 1.7-3.351 1.573-.108-1.583.506-2.694 1.25-3.561C13.292.879 14.557.16 15.769 0zm4.901 16.716v.045c-.455 1.378-1.104 2.559-1.896 3.655-.723.995-1.609 2.334-3.191 2.334-1.367 0-2.275-.879-3.676-.903-1.482-.024-2.297.735-3.652.926h-.462c-.995-.144-1.798-.932-2.383-1.642-1.725-2.098-3.058-4.808-3.306-8.276v-1.019c.105-2.482 1.311-4.5 2.914-5.478.846-.52 2.009-.963 3.304-.765.555.086 1.122.276 1.619.464.471.181 1.06.502 1.618.485.378-.011.754-.208 1.135-.347 1.116-.403 2.21-.865 3.652-.648 1.733.262 2.963 1.032 3.723 2.22-1.466.933-2.625 2.339-2.427 4.74.176 2.181 1.444 3.457 3.028 4.209z"
                   data-original="#000000" />
@@ -83,9 +86,9 @@ export default function Signup() {
             </button>
           </div>
           <div
-              class="my-8 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
+              className="my-8 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
               <p
-                class="mx-4 text-center">
+                className="mx-4 text-center">
                 Or
               </p>
             </div>
